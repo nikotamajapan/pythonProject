fb9df3af6b3671e98140ad4da415388796de69bd
mb1 <nikotamajapan@gmail.com>
2022-11-27 10:52:56 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def hi(aa, bb):
+    '''
+     
+    :param aa:
+    :param bb:
+    :return:
+    '''
+    print(aa)
+    print(bb)
+    return True
+
+
+# hi('a','b','c',d='D', e="E")
+print(hi.__doc__)
