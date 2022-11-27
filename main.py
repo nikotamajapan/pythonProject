4e97d16dfbe7b2ce7b4ac2f250895a02eb6784e5
mb1 <nikotamajapan@gmail.com>
2022-11-27 21:54:45 +0900

    57 decorator wakaran

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def add(a,b):
+def print_info(func):
+    def wrapper(*args,**kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+@print_info
+def add_num(a,b):
     return a+b
 
-r = add(10,20)
+r=add_num(10,20)
 print(r)
